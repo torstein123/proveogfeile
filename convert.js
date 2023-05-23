d20e017fb978d5f99eec07768e025705b93cde86
Torstein Olsen <tor.jetix16@gmail.com>
2023-05-23 04:43:29 +0200

    Updates

--- convert.js
+++
@@ -1,9 +0,0 @@
-
-const csvFilePath = './combined_rankings.csv';
-const csvToJson = require('csvtojson');
-
-csvToJson()
-  .fromFile(csvFilePath)
-  .then((jsonObj) => {
-    require('fs').writeFileSync('./src/combined_rankings.json', JSON.stringify(jsonObj, null, 2));
-  });
